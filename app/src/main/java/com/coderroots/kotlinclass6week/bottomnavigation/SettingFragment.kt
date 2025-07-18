4f38c325b2f00ee70e20d34a5450458a675b6dbe
rajat-cr <rajat@coderroots.com>
2025-07-18 17:31:23 +0530

    commit

--- app/src/main/java/com/coderroots/kotlinclass6week/bottomnavigation/SettingFragment.kt
+++ app/src/main/java/com/coderroots/kotlinclass6week/bottomnavigation/SettingFragment.kt
@@ -1,15 +1,11 @@
-package com.coderroots.kotlinclass6week
+package com.coderroots.kotlinclass6week.bottomnavigation
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import androidx.fragment.app.Fragment
+import com.coderroots.kotlinclass6week.R
 
 /**
  * A simple [Fragment] subclass.
@@ -18,14 +14,11 @@ private const val ARG_PARAM2 = "param2"
  */
 class SettingFragment : Fragment() {
     // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
         }
     }
 
@@ -51,8 +44,6 @@ class SettingFragment : Fragment() {
         fun newInstance(param1: String, param2: String) =
             SettingFragment().apply {
                 arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
                 }
             }
     }
